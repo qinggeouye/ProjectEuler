68d33f09f419780f4018845caf1a95b03b023442
qinggeouye <qinggeouye@gmail.com>
2019-06-06 00:34:55 +0800

    problem 28 number spiral diagonals

--- pro28.py
+++ pro28.py
@@ -1,7 +1,7 @@
 
 
 def number_spiral_diagonals(n):
-    # 左上对角线 右上对角线 右下岁角线 左下岁角线
+    # 左上对角线 右上对角线 右下对角线 左下对角线
     ans = sum((i**2 - (i-1)) + (i**2) + ((i-1)**2-(i-2)) + ((i-1)**2+1) for i in range(1, n+1, 2))
     return str(ans - 1*3)
 
