5cbb2d5bf1fcb92d1a2662aff13cdb4ad4f1a1c1
qinggeouye <qinggeouye@gmail.com>
2019-06-09 22:57:06 +0800

    problem 31 coin sums

--- pro31_2.py
+++ pro31_2.py
@@ -1,14 +1,13 @@
 
-
-def coin_sums(total):
+def coin_sums(total, coins):
 
     ways = [1] + [0] * total
     # ways[i] 总额为 i 时，所有硬币的组合（不考虑顺序）
-    for coin in [1, 2, 5, 10, 20, 50, 100, 200]:
+    for coin in coins:
         for i in range(len(ways) - coin): # 下标相差 coin
             ways[i + coin] += ways[i]  # 下标相差 coin
     return str(ways[-1])
 
 
 if __name__ == "__main__":
-    print(coin_sums(200))
+    print(coin_sums(200, [1, 2, 5, 10, 20, 50, 100, 200]))
