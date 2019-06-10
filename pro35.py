a733caa83a582a24ea076d5ebc4a2c59e410a288
qinggeouye <qinggeouye@gmail.com>
2019-06-11 00:26:01 +0800

    problem 35 circular prime -- try another

--- pro35.py
+++ pro35.py
@@ -1,3 +1,4 @@
+import time
 import eulerlib
 
 is_prime = list(map(str, eulerlib.prime_numbers.primes(999999)))  # int to str
@@ -13,28 +14,34 @@ def count():
 
 
 # second method 第二种计算
-is_prime_tmp = list(map(str, eulerlib.prime_numbers.primes(999999)))
+is_prime_tmp = list(map(str, eulerlib.prime_numbers.primes(999999)))  # int to str
 
 
 def is_circular_prime2(s):
-    return all(s[i:] + s[:i] in is_prime_tmp for i in range(len(s)))
+    return sum(1 for i in range(len(s)) if s[i:] + s[:i] in is_prime_tmp) == len(s)
+
+
+def minus_len():
+    return sum(1 for s in is_prime_tmp for i in range(1, len(s)) if s[i:] + s[:i] == s)
 
 
 def count2():
+    sub_len = minus_len()
     ans = 0
     while len(is_prime_tmp):
         s_ = is_prime_tmp[0]
         if is_circular_prime2(s_):
-            for i in range(len(s_)):
-                if s_[i:] + s_[:i] in is_prime_tmp:
-                    is_prime_tmp.remove(s_[i:] + s_[:i])
-                    ans += 1
-        else:
-            [is_prime_tmp.remove(s_[i:] + s_[:i]) for i in range(len(s_)) if s_[i:] + s_[:i] in is_prime_tmp]
+            ans += len(s_)
+        [is_prime_tmp.remove(s_[i:] + s_[:i]) for i in range(len(s_)) if s_[i:] + s_[:i] in is_prime_tmp]
 
-    return str(ans)
+    return str(ans - sub_len)
 
 
 if __name__ == "__main__":
-    # print(count())
+    start_time = time.time()
+    print(count())
+    print(time.time() - start_time)
+    start_time = time.time()
     print(count2())
+    print(time.time() - start_time)
+
