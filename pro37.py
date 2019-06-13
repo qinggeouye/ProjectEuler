dde3d0e60b6e6b8e91f2ae879a8c80a00b201974
qinggeouye <qinggeouye@gmail.com>
2019-06-14 00:25:18 +0800

    problem 37 is truncatable prime

--- pro37.py
+++ pro37.py
@@ -10,17 +10,23 @@ def is_truncatable_prime(n):
     #     if not eulerlib.is_prime(n % i):
     #         return False
     #     i *= 10
-
-    all(False for i in range(len(str(n)) + 1) if not eulerlib.is_prime(n % (10 ** i)))
+    if any(not eulerlib.is_prime(n % (10 ** i)) for i in range(1, len(str(n)) + 1)):
+        return False
 
     # test if right-truncatable
-    while n > 0:
-        if not eulerlib.is_prime(n):
-            return False
-        n //= 10
+    # while n > 0:
+    #     if not eulerlib.is_prime(n):
+    #         return False
+    #     n //= 10
+    if any(not eulerlib.is_prime(n // (10 ** i)) for i in range(len(str(n)))):
+        return False
 
     return True
 
 
+def sum_truncatable_primes():
+    return sum(itertools.islice(filter(is_truncatable_prime, itertools.count(10)), 11))
+
+
 if __name__ == "__main__":
-    print(is_truncatable_prime(15))
+    print(sum_truncatable_primes())
