43a80639ff99f0d325a376a7788a4fb766eafc9f
qinggeouye <qinggeouye@gmail.com>
2019-07-08 02:29:06 +0800

    problem 50 consecutive prime sum

---
+++ pro50.py
@@ -0,0 +1,23 @@
+import eulerlib
+
+
+def consecutive_prime_sum():
+    ans = 0
+    primes = eulerlib.prime_numbers.primes(999999)
+    consecutive = 0
+    for i, v in enumerate(primes):
+        sum_ = v
+        consecutive_tmp = 1
+        for j, val in enumerate(primes[i+1:]):
+            sum_ += val
+            consecutive_tmp += 1
+            if sum_ >= primes[-1]:
+                break
+            if sum_ in primes and consecutive_tmp > consecutive:
+                ans = sum_
+                consecutive = consecutive_tmp
+    return str(ans)
+
+
+if __name__ == "__main__":
+    print(consecutive_prime_sum())
